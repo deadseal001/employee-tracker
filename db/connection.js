3c3774abaa032ad0854429b1f4c0866b58b5333c
deadseal001 <liwenbo4936@gmail.com>
2022-11-03 22:12:21 -0500

    hide password

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,13 @@
+const { process_params } = require('express/lib/router');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '658Leiothrix.',
+    password: process.env.DB_PASSWORD,
     database: 'employee_db',
+    port:3306,
     multipleStatements: true
 });
 
